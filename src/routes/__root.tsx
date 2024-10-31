53673dda3bbe3731c3fc10ace8a2df78fd0150fc
carlssonk <o.carlssonk@gmail.com>
2024-10-31 21:02:35 +0100

    Load svg with img tag

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import GithubLogo from '../assets/github-mark-white.svg?react'
+import GithubLogo from '../assets/github-mark-white.svg'
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -8,34 +8,34 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   return (
-    <div className="flex flex-col items-center justify-center w-full text-slate-200">
-      <div className="relative flex w-full">
-        <a className="absolute top-0 flex h-full right-32" href='https://google.com' target='_blank'>Source<GithubLogo /></a>
-        <div className="flex items-center justify-center w-full border-b">
-          <div className="flex gap-3 p-2 text-lg">
-            <Link
-              to="/"
-              activeProps={{
-                className: 'font-bold',
-              }}
-              activeOptions={{ exact: true }}
-            >
-          Home
-            </Link>{' '}
-            <Link
-              to="/about"
-              activeProps={{
-                className: 'font-bold',
-              }}
-            >
-          About
-            </Link>
+      <div className="flex flex-col items-center justify-center w-full text-slate-200">
+        <div className="relative flex w-full">
+          <a className="absolute top-0 flex h-full right-32" href='https://google.com' target='_blank'>Source<img src={GithubLogo} alt="Logo" /></a>
+          <div className="flex items-center justify-center w-full border-b">
+            <div className="flex gap-3 p-2 text-lg">
+              <Link
+                to="/"
+                activeProps={{
+                  className: 'font-bold',
+                }}
+                activeOptions={{ exact: true }}
+              >
+            Home
+              </Link>{' '}
+              <Link
+                to="/about"
+                activeProps={{
+                  className: 'font-bold',
+                }}
+              >
+            About
+              </Link>
+            </div>
           </div>
         </div>
+        <div className="h-4"></div>
+        <Outlet />
+        <TanStackRouterDevtools position="bottom-right" />
       </div>
-      <div className="h-4"></div>
-      <Outlet />
-      <TanStackRouterDevtools position="bottom-right" />
-    </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import svgr from 'vite-plugin-svgr'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react(), svgr()],
+  plugins: [TanStackRouterVite(), react()],
 })
